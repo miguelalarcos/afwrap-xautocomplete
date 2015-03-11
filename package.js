8f957f089e1c469ebc528e6d671e1032406ac8c3
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-03-11 16:10:56 +0100

    prepared for version 5.0.2 of autoform

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'miguelalarcos:afwrap-xautocomplete',
   summary: 'wrap over xautocomplete so it works ok with autoform',
-  version: '0.3.0',
+  version: '0.3.1',
   git: 'https://github.com/miguelalarcos/afwrap-xautocomplete.git'
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.use('coffeescript', 'client');
   api.use('jquery', 'client');
   api.use('templating', 'client');
-  api.use('aldeed:autoform@4.2.2', 'client');
+  api.use('aldeed:autoform@5.0.2', 'client');
   api.use('miguelalarcos:xautocomplete@0.3.0', 'client');
   api.addFiles(['wrap.coffee'], 'client');
 });
